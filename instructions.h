7a2c53d8d9e968596ef208d2c2d0b8f3eb34d571
ncraluca <ralucanegoita13@gmail.com>
2024-01-07 01:58:55 +0200

    to do: rezolvare problema biblioteci in header

--- instructions.h
+++ instructions.h
@@ -1,1 +1,41 @@
-//test
+#ifndef INSTRUCTIONS_H_INCLUDED
+#define INSTRUCTIONS_H_INCLUDED
+
+#include <cstdio> //de ce nu merg bibliotecile?
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+#include <fts.h>
+#include <cerrno>
+#include <pthread.h>
+#include <cstdlib>
+#include <climits>
+#include "utils.h"
+
+void add(char *path, char *priority, char *res) {
+    // Your function implementation here
+}
+
+void suspend(int id, char *res){
+}
+
+void resume(int id, char *res){
+}
+
+void remove(int id, char *res){
+}
+
+void info(int id, char *res){
+}
+
+void list(char *res){
+}
+
+void print_id(int id, char *res){
+}
+
+void handling_instructions(char *instruction, char *res){
+
+}
+
+#endif // INSTRUCTIONS_H_INCLUDED
